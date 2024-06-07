b7f9e02fe2578baa6871b0e68ab9c6a236e9649a
Jorge <119824678+jorgessoto@users.noreply.github.com>
2024-06-07 11:42:28 -0700

    Create P1-data.js

---
+++ CIT281-Project/CIT281-P1/P1-data.js
@@ -0,0 +1,8 @@
+/*
+    CIT 281 Project 1
+    Name: Jorge Soto
+*/
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let today = new Date();
+
+console.log(days[today.getDay()]);
